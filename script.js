49dd59301cd31c0923a6b481df590e759a904a8d
goyal-aman <goyal.amanrocks@gmail.com>
2021-05-29 17:49:03 +0530

    added: Username for chat.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let chatInput = document.querySelector('.chat-input');
 let chatWindow = document.querySelector('.chat-window');
 
+let userName = prompt("Enter Your Name");
+
 chatInput.addEventListener('keypress', function(e){
     /* add 'me' user chat to chat window. */
     if(e.key=='Enter' && chatInput.value){        
         let chatDiv = document.createElement('div');
         chatDiv.classList.add("chat");
         chatDiv.classList.add("right");
-        chatDiv.textContent= chatInput.value;
+        chatDiv.textContent= userName+": "+chatInput.value;
         console.log("chat div " + chatDiv);
         chatWindow.append(chatDiv);
         chatInput.value="";
